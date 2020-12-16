d2acdf0c2639ee329900c6f51808ec06dadb360b
kevin <kevin@localhost.localdomain>
2020-12-16 22:34:13 +0800

    依赖包导入

--- files/app.py
+++ files/app.py
@@ -1,6 +1,9 @@
+# from .config.config import config
+# from .logger import register_logger
+from config.config import config
+from logger import register_logger
+
 from flask import Flask
-from ..config.config import config
-from ..logger import register_logger
 
 def create_app(deploy):
     register_logger() # 注册 日志
