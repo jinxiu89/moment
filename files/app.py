1844d8073982bbb307d61357a7a9d9d61de6a673
kevin <kevin@localhost.localdomain>
2020-12-17 22:41:54 +0800

    结构调整

--- files/app.py
+++ files/app.py
@@ -1,12 +1,18 @@
 # from .config.config import config
 # from .logger import register_logger
-from config.config import config
+from config import config
 from logger import register_logger
 
 from flask import Flask
 
+
 def create_app(deploy):
-    register_logger() # 注册 日志
+    register_logger()  # 注册 日志
     app = Flask(__name__)
     app.config.from_object(config[deploy])
     return app
+
+
+# if __name__ == '__main__':
+#     app = create_app('default')
+#     app.run()
